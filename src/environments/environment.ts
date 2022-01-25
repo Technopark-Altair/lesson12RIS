c8ec0aa2284d21934c0c5dbbe99e998051fc1af4
Kitanin Sergei <kitanin.ser@mail.ru>
2022-01-25 14:07:19 +0300

    added env link to fb db

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  fbDbUrl: process.env.FIREBASE_DB_URL || 'your_link',
 };
 
 /*
